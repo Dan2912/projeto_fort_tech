894cd4a9b0ddc2a8adc0e23d73386e350c6415c0
Danillo Rodrigues <danillo.rs@icloud.com>
2025-06-25 14:38:33 -0300

    Delete etl-bronze/escrita.py

--- etl-bronze/escrita.py
+++
@@ -1,9 +0,0 @@
-import logging
-from datetime import date
-
-def salvar_novos_dados(fs, bucket, df):
-    hoje = date.today().isoformat()
-    caminho = f"bronze/base_vendas/data={hoje}/vendas_{hoje}.parquet"
-    df.to_parquet(f"s3://{bucket}/{caminho}", index=False, engine="pyarrow", filesystem=fs)
-    logging.info(f"✅ Novos dados salvos em: s3://{bucket}/{caminho}")
-
